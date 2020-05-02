64979688ce328266762431bcfbac0145b21b0804
cpalazzi <carlo.palazzi@gmail.com>
2020-05-02 09:55:28 +0100

    Added FTFP_HP physics list source

---
+++ Hadr04-FTF-build/CMakeFiles/Hadr04.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/Hadr04.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/Hadr04.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/ActionInitialization.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/ActionInitialization.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/DetectorConstruction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/DetectorConstruction.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/DetectorMessenger.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/DetectorMessenger.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/HistoManager.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/HistoManager.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/NeutronHPMessenger.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/NeutronHPMessenger.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/NeutronHPphysics.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/NeutronHPphysics.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/PhysicsList.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/PhysicsList.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/PrimaryGeneratorAction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/PrimaryGeneratorAction.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/Run.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/Run.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/RunAction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/RunAction.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/StackingAction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/StackingAction.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/SteppingAction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/SteppingAction.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/SteppingVerbose.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/SteppingVerbose.cc.o"
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/src/TrackingAction.cc" "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-build/CMakeFiles/Hadr04.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/carlopalazzi/dev/geant4projects/Hadr04/Hadr04-FTF-source/include"
+  "/usr/local/opt/geant4/include/Geant4"
+  "/usr/local/include"
+  "/usr/local/opt/qt/lib/QtWidgets.framework"
+  "/usr/local/opt/qt/lib/QtWidgets.framework/Headers"
+  "/usr/local/opt/qt/lib/QtGui.framework"
+  "/usr/local/opt/qt/lib/QtGui.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/usr/local/opt/qt/lib/QtCore.framework"
+  "/usr/local/opt/qt/lib/QtCore.framework/Headers"
+  "/usr/local/opt/qt/./mkspecs/macx-clang"
+  "/usr/local/opt/qt/lib/QtPrintSupport.framework"
+  "/usr/local/opt/qt/lib/QtPrintSupport.framework/Headers"
+  "/usr/local/opt/qt/lib/QtOpenGL.framework"
+  "/usr/local/opt/qt/lib/QtOpenGL.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
