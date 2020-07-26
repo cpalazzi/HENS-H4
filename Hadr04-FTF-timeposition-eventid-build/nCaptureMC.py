038c1e286cadfa242c8c5d067293e2e94ffebddd
cpalazzi <carlo.palazzi@gmail.com>
2020-07-26 23:55:01 +0100

    First working ncap_sim

--- Hadr04-FTF-timeposition-eventid-build/nCaptureMC.py
+++ Hadr04-FTF-timeposition-eventid-build/nCaptureMC.py
@@ -43,21 +43,26 @@ plt.show()
 
 # %%
 def ncap_num(energy, num_n=1):
-        num_n_cap = range(40)
-        energy,num_n_cap = np.meshgrid(energy,num_n_cap)
-        # interpolate
-        grid_z0 = griddata(points, values, (energy,num_n_cap), method='linear')
-        # Replace nans with 0
-        grid_z0 = np.nan_to_num(grid_z0)
-        # Calculate area under interpolation at given energy
-        area = np.trapz(grid_z0,num_n_cap,axis=0)
-        # Normalise
-        grid_z0_norm = grid_z0/area
-        # Sample
-        choicelist = random.choices(num_n_cap, weights=grid_z0_norm, k=num_n)
-        choicelist = [int(i) for i in choicelist]
+    
+    # Split x and y data
+    points = dfenergyncap[['energy','ncapcount']].to_numpy()
+    values = dfenergyncap['eventcount'].to_numpy()
 
-        return choicelist
+    num_n_cap = range(40)
+    energy,num_n_cap = np.meshgrid(energy,num_n_cap)
+    # interpolate
+    grid_z0 = griddata(points, values, (energy,num_n_cap), method='linear')
+    # Replace nans with 0
+    grid_z0 = np.nan_to_num(grid_z0)
+    # Calculate area under interpolation at given energy
+    area = np.trapz(grid_z0,num_n_cap,axis=0)
+    # Normalise
+    grid_z0_norm = grid_z0/area
+    # Sample
+    choicelist = random.choices(num_n_cap, weights=grid_z0_norm, k=num_n)
+    choicelist = [int(i) for i in choicelist]
+
+    return choicelist
 
 
 # %%
@@ -69,25 +74,29 @@ def sample_at_data_energy(en, num=1):
     from stored data.
     """
 
-    m1, m2 = dfe.loc[dfe['energy'] == en]['rho'], dfe.loc[dfe['energy'] == en]['z']
+    m1, m2, m3 = dfe.loc[dfe['energy'] == en]['rho'], dfe.loc[dfe['energy'] == en]['z'], dfe.loc[dfe['energy'] == en]['t']
     xmin = dfe.loc[dfe['energy'] == en]['z'].min()
     xmax = dfe.loc[dfe['energy'] == en]['z'].max()
     ymin = dfe.loc[dfe['energy'] == en]['rho'].min()
     ymax = dfe.loc[dfe['energy'] == en]['rho'].max()
+    tmin = dfe.loc[dfe['energy'] == en]['t'].min()
+    tmax = dfe.loc[dfe['energy'] == en]['t'].max()
 
-    X, Y = np.mgrid[xmin:xmax:100j, ymin:ymax:100j] # makes 100 by 100 grid
-    positions = np.vstack([X.ravel(), Y.ravel()])
-    values = np.vstack([m1, m2])
+    X, Y, T = np.mgrid[xmin:xmax:100j, ymin:ymax:100j, tmin:tmax:100j] # makes 100 by 100 grid
+    positions = np.vstack([X.ravel(), Y.ravel(), T.ravel()])
+    values = np.vstack([m1, m2, m3])
     kernel = stats.gaussian_kde(values)
     Z = np.reshape(kernel(positions).T, X.shape)
 
     # Generate the bins for each axis
     x_bins = np.linspace(xmin, xmax, Z.shape[0]+1)
     y_bins = np.linspace(ymin, ymax, Z.shape[1]+1)
+    t_bins = np.linspace(tmin, tmax, Z.shape[1]+1)
 
     # Find the middle point for each bin
     x_bin_midpoints = x_bins[:-1] + np.diff(x_bins)/2
     y_bin_midpoints = y_bins[:-1] + np.diff(y_bins)/2
+    t_bin_midpoints = t_bins[:-1] + np.diff(t_bins)/2
 
     # Calculate the Cumulative Distribution Function(CDF)from the PDF
     cdf = np.cumsum(Z.ravel())
@@ -98,19 +107,21 @@ def sample_at_data_energy(en, num=1):
 
     # Find the data position
     value_bins = np.searchsorted(cdf, values)
-    x_idx, y_idx = np.unravel_index(value_bins,
+    x_idx, y_idx, t_idx = np.unravel_index(value_bins,
                                     (len(x_bin_midpoints),
-                                    len(y_bin_midpoints)))
+                                    len(y_bin_midpoints),
+                                    len(t_bin_midpoints)))
 
     # Create the new data
     new_data = np.column_stack((x_bin_midpoints[x_idx],
-                                y_bin_midpoints[y_idx]))
-    new_x, new_y = new_data.T
+                                y_bin_midpoints[y_idx],
+                                t_bin_midpoints[t_idx]))
+    new_x, new_y, new_z = new_data.T
 
     return new_data
 
 # %%
-def sample_rho_z(energy, num=1):
+def interp_rho_z_t(energy, num=1):
 
     """
     Gives samples of rho and z at given energy based on values
@@ -125,17 +136,18 @@ def sample_rho_z(energy, num=1):
     samplearr1 = sample_at_data_energy(en1,num)
     samplearr2 = sample_at_data_energy(en2,num)
     i = 0
-    listOutRhoZ = []
+    list_out_rho_z = []
     while i < np.shape(samplearr1)[0]:
         new_data1 = samplearr1[i]
         new_data2 = samplearr2[i] 
-        valuestointerp = np.vstack([new_data1, new_data2]).transpose()
-        listOutRhoZ.append(griddata([en1, en2], valuestointerp, energy, method='linear', rescale=True))
+        values_to_interp = np.vstack([new_data1, new_data2])#.transpose()
+        list_out_rho_z.append(griddata([en1, en2], values_to_interp, energy, method='linear', rescale=True))
         i+=1
 
-    dfOutRhoZ = pd.DataFrame(listOutRhoZ, columns=['rho', 'z'])
+    df_out_rho_z = pd.DataFrame(np.row_stack(list_out_rho_z), columns=['rho', 'z', 't'])
 
-    return dfOutRhoZ
+    return df_out_rho_z
+
 
 
 # %%
@@ -144,9 +156,9 @@ def ncap_sim(energy, num_n=1):
     # Get number of ncaps
     num_ncaps = sum(ncap_num(energy, num_n))
     # Get positions
-    return sample_rho_z(energy, num_ncaps)
+    return interp_rho_z_t(energy, num_ncaps)
 
 # %%
-ncap_sim(1666)
+ncap_sim(659)
 
 # %%
